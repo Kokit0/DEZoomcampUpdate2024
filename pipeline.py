53fdfe0ce220f0465866880cb46657caaca71333
Kokke <86896405+Kokit0@users.noreply.github.com>
2024-12-12 19:13:35 +0000

    updated to dockerfile and pipeline copy to container process

---
+++ pipeline.py
@@ -0,0 +1,11 @@
+import sys
+
+import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv [1]
+
+# hagamos fancy stuffs con pandas como cargar CSV files, etc.
+
+print(f'job finished succesfully for day = {day}')
